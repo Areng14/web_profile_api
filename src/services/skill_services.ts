cc7ddd659ebe7986cc05fb9c99bbf510ed5492cc
ArengDev <kktdell.1@gmail.com>
2025-01-21 16:53:02 +0700

    skill services

--- src/services/skill_services.ts
+++ src/services/skill_services.ts
@@ -9,4 +9,25 @@ export class SkillService {
         this.skills.set(skilldata.id, skilldata)
         return skilldata
     }
+
+    async getById(id: string): Promise<Skill | null> {
+        return this.skills.get(id) || null
+    }
+
+    async getALl(): Promise<Skill[]> {
+        return Array.from(this.skills.values())
+    }
+
+    async updateSkill(id: string, data: CreateSkillDTO): Promise<Skill | null> {
+        const skill = this.skills.get(id)
+        if (!skill) {
+            return null
+        }
+        Object.assign(skill, data)
+        return skill
+    }
+
+    async deleteSkill(id: string): Promise<boolean> {
+        return this.skills.delete(id)
+    }
 }
