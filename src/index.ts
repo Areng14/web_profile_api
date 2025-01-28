81803397b89e095dec84cae046f7bcc448b220a0
ArengDev <kktdell.1@gmail.com>
2025-01-28 16:46:54 +0700

    made run on port

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import routes from "./routes";
 
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
 
