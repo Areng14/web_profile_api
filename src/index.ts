921441181bc8bc5bfe158089bfacc125d91058ce
waritk <warit.karnbunjob@gmail.com>
2025-02-18 16:39:15 +0700

    mongo connect

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import routes from "./routes";
+import mongoose from "mongoose";
 
 require("dotenv").config();
 const app = express();
@@ -8,6 +9,11 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+mongoose
+  .connect(`${process.env.MONGO_URL}`)
+  .then(() => console.log("Conntect to mongoDBB"))
+  .catch((err) => console.error(`Mongo Connection error : ${err}`));
 
 app.get("/", (req: Request, res: Response) => {
   res.json({ message: "Hello World!" });
