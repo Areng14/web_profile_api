1dcd77f1572e8fbbd67f5ea6f298a4e98a825997
waritk <warit.karnbunjob@gmail.com>
2025-06-03 16:11:52 +0700

    init branch

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,18 @@ import { Router } from "express";
 import skillRoutes from "./skillRoutes";
 import projectRoutes from "./projectRoutes";
 import userRoutes from "./userRoutes";
+import { requireAuth } from "../middlewares/auth";
+import publicRoutes from "./public";
 
 const router = Router();
 
-router.use("/", skillRoutes);
-router.use("/", projectRoutes);
+router.get("/health", (req, res) => {
+  res.json({ status: "ok" });
+});
+
+router.use("/public/", publicRoutes);
 router.use("/", userRoutes);
+router.use("/", requireAuth, skillRoutes);
+router.use("/", projectRoutes);
 
 export default router;
